c24efbe4396456a5489fc54540573ff1413184a4
yelnurimash <yelnurimash@gmail.com>
2024-11-15 18:45:57 +0500

    1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        
+        Hello wolrd
+      </div>
+    </>
+  )
+}
+
+export default App
